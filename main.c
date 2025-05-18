8f192edb192caac688a719da24271ed3058fbf21
thinh06 <thinh06@desktop-mr25726.local>
2025-05-18 19:24:08 +0700

    khai bao don gian

--- main.c
+++ main.c
@@ -4,6 +4,17 @@
 /*  Su dung struct de lam bai tap quan ly sinh vien */
 
 int main(){
-    printf("Hello, world! and to git RCM~! \n");
+    int n;
+    struct sinhVien
+    {
+        char hoTen;
+        int namSinh;
+        int gioiTinh;
+        float cc, kT, dT, dKT;
+    };
+    
+    printf("Cho biet so sinh vien: ");
+    scanf("%d", &n);
+
     return 0;
 }
