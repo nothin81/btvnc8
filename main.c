f7dcad45ebff9e9690333c2efddd71632f58574a
thinh06 <thinh06@desktop-mr25726.local>
2025-05-18 21:51:43 +0700

    commit thieu --> da them lai

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <string.h>
+/*  Su dung struct de lam bai tap quan ly sinh vien  */
 
-/*  Su dung struct de lam bai tap quan ly sinh vien */
+typedef struct
+{
+    char hoTen[40];
+    int namSinh;
+    int gioiTinh;
+    float cc, kT, dT, dTK;
+} sinhVien;
 
-int main(){
-    int n;
-    struct sinhVien
-    {
-        char hoTen;
-        int namSinh;
-        int gioiTinh;
-        float cc, kT, dT, dKT;
+void nhapLieu(sinhVien SV[99], int n){
+    int i;
+    for (i=0; i<=n-1; i++){
+        printf("Nhap vao ho va ten cua sinh vien thu %d: ", i+1);
+        getchar();  //loai new line identifier trong buffer stdin
+        fgets(SV[i].hoTen, 30, stdin);
+
+        printf("\nNhap vao nam sinh cua sinh vien thu %d: ", i+1);
+        scanf("%d", &SV[i].namSinh);
+
+        printf("Moi nhap gioi tinh cua sinh vien so %d: ", i+1);
+        scanf("%d",&SV[i].gioiTinh);
+
+        printf("Moi nhap DCC/DKT/DThi: ");
+        scanf("%f%f%f", &SV[i].cc, &SV[i].kT, &SV[i].dT);
+
     };
-    
-    printf("Cho biet so sinh vien: ");
-    scanf("%d", &n);
+}
 
-    return 0;
+void tinhToanDiem(){
+
 }
